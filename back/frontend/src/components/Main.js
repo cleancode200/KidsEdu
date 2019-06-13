0a70d87532ba32e7df63c036a95d83a9e170c4ed
Amer-Alkharashgah <buildhorozon@gmail.com>
2019-06-13 17:24:53 +0300

    signin component

---
+++ back/frontend/src/components/Main.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+export class Main extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <div>
+                <Link to="/signin">
+                  <button>Sign In</button>
+                </Link>
+                <Link to="/signup">
+                  <button>Sign Up</button>
+                </Link>
+              </div>
+            )}
+          />
+
+         
+          <Route path="/signin" component={SignIn} />
+          <Route path="/signUp" component={SignUp} />
+          <Route exact path="/Main" component={Main} />
+         
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default Main;
