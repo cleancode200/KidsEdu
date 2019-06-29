b80433011cec9d78780e06c9bcb6ee5a2ab32d30
yousefalshabahm <yousefalshabah@hotmail.com>
2019-06-29 17:45:03 +0300

    delete the remove button from addchild comp

--- back/frontend/src/components/AddChild.js
+++ back/frontend/src/components/AddChild.js
@@ -145,18 +145,6 @@ export class AddChild extends Component {
     });
   }
 
-  removeChild(index) {
-    console.log(this.state.childInfo[index].id);
-
-    if (this.state.childInfo[index].id) {
-      axios
-        .delete(`/Child/${this.state.childInfo[index].id}`)
-        .then(res => {
-          console.log(res.data);
-        });
-    }
-  }
-
   render() {
     const style = {
       color: "#006e96"
@@ -253,17 +241,6 @@ export class AddChild extends Component {
                       <p className="agePara"> Age : {child.age}</p>
                     </div>
                   </div>
-                  <button
-                    type="button"
-                    className="linkbtn btn btn-link"
-                    id="name"
-                    key={index}
-                    onClick={() => {
-                      this.removeChild(index);
-                    }}
-                  >
-                    remove
-                  </button>
                 </div>
               </div>
             );
